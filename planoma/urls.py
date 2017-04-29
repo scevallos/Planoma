d0d8b76b98ff1ab01737e66e6eec3d74ed279a20
Sebastian <slc02014@mymail.pomona.edu>
2017-04-29 16:40:37 -0700

    forgot url

--- planoma/urls.py
+++ planoma/urls.py
@@ -41,5 +41,5 @@ urlpatterns = [
 
 # if not settings.DEBUG:
 urlpatterns += (
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
